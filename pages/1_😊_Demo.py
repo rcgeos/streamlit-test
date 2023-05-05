d408778c1cfef14a4f0668d7c7c1f4ca127336d5
rcgeos <renato.cumani4@gmail.com>
2023-05-05 10:15:03 -0400

    modifed order

--- pages/1_😊_Demo.py
+++ pages/1_😊_Demo.py
@@ -5,6 +5,11 @@ import leafmap.foliumap as leafmap
 #m = pyrcgeos.Map()
 
 st.set_page_config(layout="wide")
+dropdown = st.selectbox("Basemap",["HYBRID","ROADMAP","TERRAIN","SATELLITE"])
+
 m = leafmap.Map()
-m.add_basemap('HYBRID')
-m.to_streamlit()
+m.add_basemap(dropdown)
+# m.add_basemap('HYBRID')
+# m.to_streamlit()
+
+
