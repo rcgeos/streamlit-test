3ee6502ad19048ff67d4af9b153a409414e80400
rcgeos <renato.cumani4@gmail.com>
2023-05-05 10:46:22 -0400

    modified pyrcgeos

--- pages/1_😊_Demo.py
+++ pages/1_😊_Demo.py
@@ -1,13 +1,13 @@
 import streamlit as st
 import leafmap.foliumap as leafmap 
-#import pyrcgeos.foliumap as pyrcgeos
+import pyrcgeos.foliumap as pyrcgeos
 
-#m = pyrcgeos.Map()
+m = pyrcgeos.Map()
 
 st.set_page_config(layout="wide")
 dropdown = st.selectbox("Basemap",["HYBRID","ROADMAP","TERRAIN","SATELLITE"])
 
-m = leafmap.Map()
+#m = leafmap.Map()
 m.add_basemap(dropdown)
 # m.add_basemap('HYBRID')
 m.to_streamlit()
