c01ccb62eb5a4edf70519916dc63d205ab63f085
rcgeos <renato.cumani4@gmail.com>
2023-05-05 11:45:33 -0400

    add url

--- pages/1_😊_Demo.py
+++ pages/1_😊_Demo.py
@@ -8,11 +8,12 @@ st.set_page_config(layout="wide")
 col1, col2  = st.columns([4,1]) #80% col1 and 20% col2
 with col2: 
     dropdown = st.selectbox("Basemap",["HYBRID","ROADMAP","TERRAIN","SATELLITE"])
+    url = st.text_input("Enter Url")
 
 m = leafmap.Map()
 m.add_basemap(dropdown)
-    # m.add_basemap('HYBRID')
-    
+if url: 
+    m.add_tile_layer(url, name='Tile Layer', attribution='')    # m.add_basemap('HYBRID')
 
 with col1: 
    m.to_streamlit() # this should be the last line 
