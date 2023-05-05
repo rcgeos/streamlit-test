27087dffa95c837bd07377a3bb744f86d8615734
rcgeos <renato.cumani4@gmail.com>
2023-05-05 11:41:08 -0400

    modified columns

--- pages/1_😊_Demo.py
+++ pages/1_😊_Demo.py
@@ -6,11 +6,13 @@ import leafmap.foliumap as leafmap
 st.set_page_config(layout="wide")
 
 col1, col2  = st.columns([4,1]) #80% col1 and 20% col2
-with col1: 
-    m = leafmap.Map()
-    m.add_basemap(dropdown)
-    # m.add_basemap('HYBRID')
-    m.to_streamlit()
-
 with col2: 
     dropdown = st.selectbox("Basemap",["HYBRID","ROADMAP","TERRAIN","SATELLITE"])
+
+m = leafmap.Map()
+m.add_basemap(dropdown)
+    # m.add_basemap('HYBRID')
+    
+
+with col1: 
+   m.to_streamlit() # this should be the last line 
