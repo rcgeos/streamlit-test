f23f9ff534990819b7d566905eb9b67064d83b70
rcgeos <renato.cumani4@gmail.com>
2023-05-05 10:07:47 -0400

    add basemap

--- pages/1_😊_Demo.py
+++ pages/1_😊_Demo.py
@@ -3,5 +3,8 @@ import leafmap.foliumap as leafmap
 #import pyrcgeos.foliumap as pyrcgeos
 
 #m = pyrcgeos.Map()
+
+st.set_page_config(layout="wide")
 m = leafmap.Map()
+m.add_basemap('HYBRID')
 m.to_streamlit()
