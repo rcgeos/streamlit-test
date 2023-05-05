035acce2a604d691f30efeb9bd2566a4799ac9bc
rcgeos <renato.cumani4@gmail.com>
2023-05-05 11:54:30 -0400

    updated parameter

--- pages/1_😊_Demo.py
+++ pages/1_😊_Demo.py
@@ -7,7 +7,7 @@ st.set_page_config(layout="wide")
 
 col1, col2  = st.columns([4,1]) #80% col1 and 20% col2
 
-options = list(leafmap.basemap.keys())
+options = list(leafmap.basemaps.keys())
 
 with col2: 
     dropdown = st.selectbox("Basemap",options)
