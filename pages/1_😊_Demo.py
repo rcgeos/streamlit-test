ec301f80a87c9e5f8ca3870651de8f6ba2951913
rcgeos <renato.cumani4@gmail.com>
2023-05-05 11:53:15 -0400

    added options url

--- pages/1_😊_Demo.py
+++ pages/1_😊_Demo.py
@@ -6,8 +6,12 @@ import leafmap.foliumap as leafmap
 st.set_page_config(layout="wide")
 
 col1, col2  = st.columns([4,1]) #80% col1 and 20% col2
+
+options = list(leafmap.basemap.keys())
+
 with col2: 
-    dropdown = st.selectbox("Basemap",["HYBRID","ROADMAP","TERRAIN","SATELLITE"])
+    dropdown = st.selectbox("Basemap",options)
+    #dropdown = st.selectbox("Basemap",["HYBRID","ROADMAP","TERRAIN","SATELLITE"])
     url = st.text_input("Enter Url")
 
 m = leafmap.Map()
